0b91aa33df8551b74ddfe036577532e6dc408915
john <cdkd12345@gmail.com>
2024-03-22 16:33:03 +0800

    add rflutter alert

--- lib/quizzler.dart
+++ lib/quizzler.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:xylophone/model/quizz_brain.dart';
 import 'model/question.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class Quizzler extends StatefulWidget {
   @override
@@ -10,9 +12,9 @@ class Quizzler extends StatefulWidget {
 }
 
 class QuizzlerState extends State<Quizzler> {
-  final List<Question> questionList = [];
-  late int questionNum = 0;
+
   List<FinalResult> resultList = [];
+  QuizzBrain brain = QuizzBrain();
 
   @override
   void initState() {
@@ -22,55 +24,87 @@ class QuizzlerState extends State<Quizzler> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData.dark(useMaterial3: true),
-        home: Scaffold(
-            body: SafeArea(
-                child: Column(
+    return Scaffold(
+        body: SafeArea(
+            child: Column(
+      children: [
+        Expanded(
+          flex: 5,
+          child: Center(
+            child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(brain.getCurDescription())),
+          )),
+        Column(
           children: [
-            Expanded(flex: 5,
-                child: Center(child: Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(questionList[questionNum].questionDescription)),)),
-            Column(children: [
-                Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    QuestionButton(() => onHit(true), 'True', Colors.red),
-                    QuestionButton(()=> onHit(false), 'False', Colors.green),
-                  ]),
-                ),
-            ],),
-            generResult(resultList)
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  QuestionButton(
+                      () => onHit(true, context), 'True', Colors.red),
+                  QuestionButton(
+                      () => onHit(false, context), 'False', Colors.green),
+                ]),
+            ),
           ],
-        ))));
+        ),
+        generResult(resultList)
+      ],
+    )));
   }
 
   void initQuestions() {
-    questionList.addAll([
-      Question("1+1 = 3，正确吗？", false),
-      Question("1+2 = 3，正确吗？", true),
-      Question("1+3 = 3，正确吗？", false),
-      Question("1+4 = 3，正确吗？", false),
-      Question("1+5 = 3，正确吗？", false)
-    ]);
+    brain.init();
   }
 
-  void onHit(bool result) {
+  void onHit(bool result, BuildContext context) {
+    if (brain.getCurIndex() == 0) {
+      resultList.clear();
+    }
     setState(() {
-      if (questionNum == 0) {
-        resultList.clear();
-      }
-      if (questionList[questionNum].anser == result) {
+      if (brain.getCurAnswer() == result) {
         resultList.add(FinalResult(true, Icons.check));
       } else {
         resultList.add(FinalResult(false, Icons.close_rounded));
       }
-      questionNum = (questionNum + 1) % questionList.length;
-      print("questionNum:$questionNum");
+
+      print("questionNum:${brain.getCurIndex()}");
+      brain.nextQuestion();
     });
+
+    if (brain.getCurIndex() == 0) {
+      _onCustomAnimationAlertPressed(context);
+    }
+  }
+
+  _onCustomAnimationAlertPressed(context) {
+    final Alert alert = Alert(
+        context: context,
+        title: "tips",
+        desc: "题目已经全部回答完毕，重新作答吗？",
+        alertAnimation: fadeAlertAnimation,
+        onWillPopActive: true,
+        buttons: [
+          DialogButton(child: Text('Yes'), color: Colors.green, onPressed: () => nextStep(context),),
+          DialogButton(child: Text('No'), onPressed: () => nextStep(context))
+        ]);
+    alert.show();
+  }
+
+  Widget fadeAlertAnimation(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return Align(
+      child: FadeTransition(
+        opacity: animation,
+        child: child,
+      ),
+    );
   }
 
   Widget generResult(List<FinalResult> resultList) {
@@ -78,15 +112,21 @@ class QuizzlerState extends State<Quizzler> {
         child: resultList.isNotEmpty
             ? ListView.builder(
                 scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) =>
-                    Icon(resultList[index].resultIcon, color: resultList[index].result? Colors.white: Colors.red),
+                itemBuilder: (context, index) => Icon(
+                    resultList[index].resultIcon,
+                    color:
+                        resultList[index].result ? Colors.white : Colors.red),
                 itemCount: resultList.length)
             : Text('empty'));
   }
+
+  void nextStep(BuildContext context) {
+    Navigator.pop(context);
+  }
+
 }
 
 class QuestionButton extends StatelessWidget {
-
   final VoidCallback? onPressed;
   final String title;
   final Color? color;
@@ -100,10 +140,8 @@ class QuestionButton extends StatelessWidget {
           child: Text(title, style: TextStyle(color: Colors.white)),
           onPressed: onPressed,
           style: ButtonStyle(
-              backgroundColor:
-              MaterialStateProperty.all<Color>(color!),
-              shape:
-              MaterialStateProperty.all<RoundedRectangleBorder>(
+              backgroundColor: MaterialStateProperty.all<Color>(color!),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(0),
                       side: BorderSide(color: color!))))),
